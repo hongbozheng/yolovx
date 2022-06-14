6ed6b099132a8a2f2288d0e30b41fe1d8a850a29
Hongbo Zheng <hongboz2@illinois.edu>
2022-06-13 21:53:57 -0500

    :white_check_mark: parse.py working :white_check_mark:

--- src/parse.py
+++ src/parse.py
@@ -21,29 +21,29 @@ def parse_cfg(cfg):
 
     def parse_cfg_parameter():
         try:
-            block[key.rstrip()] = int(value)
-            logging.debug('[try int]: {}={}'.format(key.rstrip(),block[key.rstrip()]))
-            logging.debug('[Variable Type]: {}'.format(type(block[key.rstrip()])))
+            block[key] = int(value)
+            logging.debug('[try int]  : {}={}'.format(key,block[key]))
+            logging.debug('[Var Type] : {}'.format(type(block[key])))
         except:
             try:
-                block[key.rstrip()] = float(value)
-                logging.debug('[try float]: {}={}'.format(key.rstrip(),block[key.rstrip()]))
-                logging.debug('[Variable Type]: {}'.format(type(block[key.rstrip()])))
+                block[key] = float(value)
+                logging.debug('[try float]: {}={}'.format(key,block[key]))
+                logging.debug('[Var Type] : {}'.format(type(block[key])))
             except:
-                block[key.rstrip()] = value.lstrip().split(',')
+                block[key] = value.lstrip().split(',')
                 try:
-                    block[key.rstrip()] = [int(x) for x in block[key.rstrip()]]
-                    logging.debug('[try int list]: {}={}'.format(key.rstrip(),block[key.rstrip()]))
-                    logging.debug('[Variable Type list]: {}'.format(type(block[key.rstrip()][0])))
+                    block[key] = [int(x) for x in block[key]]
+                    logging.debug('[try int list] : {}={}'.format(key,block[key]))
+                    logging.debug('[Var Type list]: {}'.format(type(block[key][0])))
                 except:
                     try:
-                        block[key.rstrip()] = [float(x) for x in block[key.rstrip()]]
-                        logging.debug('[try float list]: {}={}'.format(key.rstrip(),block[key.rstrip()]))
-                        logging.debug('[Variable Type list]: {}'.format(type(block[key.rstrip()][0])))
+                        block[key] = [float(x) for x in block[key]]
+                        logging.debug('[try float list]: {}={}'.format(key,block[key]))
+                        logging.debug('[Var Type list] : {}'.format(type(block[key][0])))
                     except:
-                        block[key.rstrip()] = value.lstrip()
-                        logging.debug('[try str]: {}={}'.format(key.rstrip(),block[key.rstrip()]))
-                        logging.debug('[Variable Type]: {}'.format(type(block[key.rstrip()])))
+                        block[key] = value.lstrip()
+                        logging.debug('[try str]  : {}={}'.format(key,block[key]))
+                        logging.debug('[Var Type] : {}'.format(type(block[key])))
 
     file = open(cfg,'r')
     lines = file.read().split('\n')
@@ -60,41 +60,42 @@ def parse_cfg(cfg):
     for line in lines:
         if line == '[net]':
             layer_type = LayerType.net
-            logging.debug('[Layer Type]: ----- net -----')
+            logging.debug('[LayerType]: ----- net -----')
             block['type'] = 'net'
             continue
         if line == '[convolutional]':
             append_block() 
             layer_type = LayerType.convolutional
-            logging.debug('[Layer Type]: ----- convolutional -----')
+            logging.debug('[LayerType]: ----- convolutional -----')
             block['type'] = 'convolutional'
             continue
         if line == '[shortcut]':
             append_block()
             layer_type = LayerType.shortcut
-            logging.debug('[Layer Type]: ----- shortcut -----')
+            logging.debug('[LayerType]: ----- shortcut -----')
             block['type'] = 'shortcut'
             continue
         if line == '[route]':
             append_block()
             layer_type = LayerType.route
-            logging.debug('[Layer Type]: ----- route -----')
+            logging.debug('[LayerType]: ----- route -----')
             block['type'] = 'route'
             continue
         if line == '[upsample]':
             append_block()
             layer_type = LayerType.upsample
-            logging.debug('[Layer Type]: ----- upsample -----')
+            logging.debug('[LayerType]: ----- upsample -----')
             block['type'] = 'upsample'
             continue
         if line == '[yolo]':
             append_block()
             layer_type = LayerType.yolo
-            logging.debug('[Layer Type]: ----- yolo -----')
+            logging.debug('[LayerType]: ----- yolo -----')
             block['type'] = 'yolo'
             continue
 
         key,value = line.split('=')
+        key = key.rstrip()
 
         if layer_type == LayerType.net:    
             parse_cfg_parameter()
@@ -108,11 +109,11 @@ def parse_cfg(cfg):
             parse_cfg_parameter()
         elif layer_type == LayerType.yolo:
             parse_cfg_parameter()
-            if key.rstrip() == 'anchors':
+            if key == 'anchors':
                 block['anchors'] = [(block['anchors'][i],block['anchors'][i+1]) 
                                     for i in range(0,len(block['anchors']),2)]
-                logging.debug('[tuple]: {}'.format(block['anchors']))
-                logging.debug('[Varibale Type]: {}'.format(type(block['anchors'][0])))
+                logging.debug('[tuple]    : {}'.format(block['anchors']))
+                logging.debug('[Var Type] : {}'.format(type(block['anchors'][0])))
         else:
             logging.error('[ERROR]: INVALID LAYER TYPE!')
             assert False
