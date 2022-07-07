469cb4ecd56c440f4c85681e1405473bc2cc5df1
Hongbo Zheng <hongboz2@illinois.edu>
2022-07-07 03:10:57 -0500

    :ambulance: HUGE FIX :ambulance:

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,4 @@
 import logging
-from enum import IntEnum
 import torch
 import numpy as np
 
@@ -8,126 +7,64 @@ epsilon = 1e-6
 '''
 $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ Parse YOLO cfg file $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 '''
-class LayerType(IntEnum):
-    net           = 0
-    convolutional = 1
-    shortcut      = 2
-    route         = 3
-    upsample      = 4
-    yolo          = 5
-
 def parse_cfg(cfg):
-
-    def append_block():
-        nonlocal block
-        if len(block) != 0:
-            blocks.append(block)
-            block = {}
-
-    def parse_cfg_parameter():
-        try:
-            block[key] = int(value)
-            logging.debug('[try int]  : {}={}'.format(key,block[key]))
-            logging.debug('[Var Type] : {}'.format(type(block[key])))
-        except:
-            try:
-                block[key] = float(value)
-                logging.debug('[try float]: {}={}'.format(key,block[key]))
-                logging.debug('[Var Type] : {}'.format(type(block[key])))
-            except:
-                block[key] = value.lstrip().split(',')
-                try:
-                    block[key] = [int(x) for x in block[key]]
-                    logging.debug('[try int list] : {}={}'.format(key,block[key]))
-                    logging.debug('[Var Type list]: {}'.format(type(block[key][0])))
-                except:
-                    try:
-                        block[key] = [float(x) for x in block[key]]
-                        logging.debug('[try float list]: {}={}'.format(key,block[key]))
-                        logging.debug('[Var Type list] : {}'.format(type(block[key][0])))
-                    except:
-                        block[key] = value.lstrip()
-                        logging.debug('[try str]  : {}={}'.format(key,block[key]))
-                        logging.debug('[Var Type] : {}'.format(type(block[key])))
-
+    
     file = open(cfg,'r')
     lines = file.read().split('\n')
     lines = [x for x in lines if len(x) > 0]
     lines = [x for x in lines if x[0] != '#']
     
-    block = {}
-    blocks = []
+    layer_config = {}
+    configuration = []
     
     logging.debug('[cfg lines]: {}'.format(lines))
 
-    layer_type = -1
-
     for line in lines:
-        if line == '[net]':
-            layer_type = LayerType.net
-            logging.debug('[LayerType]: ----- net -----')
-            block['type'] = 'net'
-            continue
-        if line == '[convolutional]':
-            append_block() 
-            layer_type = LayerType.convolutional
-            logging.debug('[LayerType]: ----- convolutional -----')
-            block['type'] = 'convolutional'
-            continue
-        if line == '[shortcut]':
-            append_block()
-            layer_type = LayerType.shortcut
-            logging.debug('[LayerType]: ----- shortcut -----')
-            block['type'] = 'shortcut'
-            continue
-        if line == '[route]':
-            append_block()
-            layer_type = LayerType.route
-            logging.debug('[LayerType]: ----- route -----')
-            block['type'] = 'route'
-            continue
-        if line == '[upsample]':
-            append_block()
-            layer_type = LayerType.upsample
-            logging.debug('[LayerType]: ----- upsample -----')
-            block['type'] = 'upsample'
-            continue
-        if line == '[yolo]':
-            append_block()
-            layer_type = LayerType.yolo
-            logging.debug('[LayerType]: ----- yolo -----')
-            block['type'] = 'yolo'
-            continue
-
-        key,value = line.split('=')
-        key = key.rstrip()
-
-        if layer_type == LayerType.net:    
-            parse_cfg_parameter()
-        elif layer_type == LayerType.convolutional:
-            parse_cfg_parameter()
-        elif layer_type == LayerType.shortcut:
-            parse_cfg_parameter()
-        elif layer_type == LayerType.route:
-            parse_cfg_parameter()
-        elif layer_type == LayerType.upsample:
-            parse_cfg_parameter()
-        elif layer_type == LayerType.yolo:
-            parse_cfg_parameter()
-            if key == 'anchors':
-                block['anchors']= [(block['anchors'][i],block['anchors'][i+1]) 
-                                    for i in range(0,len(block['anchors']),2)]
-                logging.debug('[tuple]    : {}'.format(block['anchors']))
-                logging.debug('[Var Type] : {}'.format(type(block['anchors'][0])))
+        if line[0] == '[':
+            if len(layer_config) != 0:
+                configuration.append(layer_config)
+                layer_config = {}
+            layer_config['type'] = line[1:-1]
         else:
-            logging.error('[ERROR]: INVALID LAYER TYPE!')
-            assert False
+            key,value = line.split('=')
+            key = key.rstrip()
+        
+            try:
+                layer_config[key] = int(value)
+                logging.debug('[try int]  : {}={}'.format(key,layer_config[key]))
+                logging.debug('[Var Type] : {}'.format(type(layer_config[key])))
+            except:
+                try:
+                    layer_config[key] = float(value)
+                    logging.debug('[try float]: {}={}'.format(key,layer_config[key]))
+                    logging.debug('[Var Type] : {}'.format(type(layer_config[key])))
+                except:
+                    layer_config[key] = value.lstrip().split(',')
+                    try:
+                        layer_config[key] = [int(x) for x in layer_config[key]]
+                        logging.debug('[try int list] : {}={}'.format(key,layer_config[key]))
+                        logging.debug('[Var Type list]: {}'.format(type(layer_config[key][0])))
+                    except:
+                        try:
+                            layer_config[key] = [float(x) for x in layer_config[key]]
+                            logging.debug('[try float list]: {}={}'.format(key,layer_config[key]))
+                            logging.debug('[Var Type list] : {}'.format(type(layer_config[key][0])))
+                        except:
+                            layer_config[key] = value.lstrip()
+                            logging.debug('[try str]  : {}={}'.format(key,layer_config[key]))
+                            logging.debug('[Var Type] : {}'.format(type(layer_config[key])))
+        
+            if key == 'anchors':
+                layer_config['anchors'] = [(layer_config['anchors'][i],layer_config['anchors'][i+1]) 
+                                    for i in range(0,len(layer_config['anchors']),2)]
+                logging.debug('[tuple]    : {}'.format(layer_config['anchors']))
+                logging.debug('[Var Type] : {}'.format(type(layer_config['anchors'][0])))
 
-    append_block()
-    logging.debug('[blocks]: {}'.format(blocks))
-    print('[INFO]: Finish parsing cfg file')
-    print('[len(blocks)]: [Net]={} + [Layer]={}'.format(1,len(blocks)-1))
-    return blocks
+    configuration.append(layer_config)
+    logging.debug('[blocks]: {}'.format(layer_config))
+    print('[INFO]:   Finish parsing cfg file')
+    print('[CONFIG]: [Net]={} + [Layer]={}'.format(1,len(configuration)-1))
+    return configuration
 
 '''
 $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ detection post processing $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
